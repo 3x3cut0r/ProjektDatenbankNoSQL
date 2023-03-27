9f0d65071db1064a9ea381aaefd4f35491a450f9
3x3cut0r <executor55@gmx.de>
2023-03-27 12:59:24 +0200

    test updated

--- api/controllers/test.js
+++ api/controllers/test.js
@@ -3,48 +3,6 @@ const Document = require('../models/document');
 const JSend = require('../utils/jsend');
 
 exports.getTest = async (req, res, next) => {
-  /* 
-  // add
-  let newDocument = await Document.save({
-    collection: 'items',
-    document: { name: 'Handbag', price: '34,95' },
-  });
-  console.log('🚀 - exports.getTest= - newDocument', newDocument);
-
-  // update
-  newDocument.document.price = '39,99';
-  const updatedDocument = await newDocument.save();
-  console.log('🚀 - exports.getTest= - updatedDocument', updatedDocument);
-
-  // get
-  const singleDocument = await Document.get({
-    collection: 'items',
-    key: 'price',
-    value: '39,99',
-  });
-  console.log('🚀 - exports.getTest= - singleDocument', singleDocument);
-
-  // getbyID
-  const singleDocumentByID = await Document.getByID({
-    collection: 'items',
-    _id: '63d3a8b814c49e703d25fb4f',
-  });
-  console.log('🚀 - exports.getTest= - singleDocumentByID', singleDocumentByID);
-
-  // delete
-  const deletedDocument = await newDocument.delete();
-  console.log('🚀 - exports.getTest= - deletedDocument', deletedDocument);
-
-  // fetchAll
-  const allItems = await Document.fetchAll('items');
- */
-
-  const doc = await Document.findRetweet({
-    collection: 'tweets',
-    key: '_id',
-    value: '63d3bcec7a1a6c9fad78efa0',
-  });
-
   JSend.success(res, { code: 200 /*  data: allItems */ });
 };
 
